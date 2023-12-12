df21dcc536f563bdc5645861d30cb03d5d7ce4a5
Rifki Dimas <ddimarifki@gmail.com>
2023-12-12 23:11:30 +0700

    my doa-app

---
+++ app/src/main/java/com/example/mydoaapp/SplashScreen.java
@@ -0,0 +1,35 @@
+package com.example.mydoaapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        getSupportActionBar().hide();
+
+        // ini fungsi untuk progressbarnya
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try{
+                    Thread.sleep(3000); // Waktu Pnding 3 Detik
+                }catch(InterruptedException ex){
+                    ex.printStackTrace();
+                }finally {
+                    startActivity(new Intent(SplashScreen.this, MainActivity.class));
+                    finish();
+                }
+            }
+        });
+        thread.start();
+        // end fungsi
+
+
+    }
+}
